a97c691b14355281110741d61efed5043f3beb71
archiehull <76594364+archiehull@users.noreply.github.com>
2025-03-28 18:41:27 +0000

    training
    may need to downgrade to py3.6

--- Fundimentals_OOP.py
+++ Fundimentals_OOP.py
@@ -31,6 +31,10 @@ from keras.applications.xception import Xception
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.optimizers import Adam
 from keras.models import Model
+from keras.callbacks import TensorBoard
+
+from keras.backend.tensorflow_backend as backend
+from threading import Threading
 
 # CONSTANTS
 
@@ -60,6 +64,38 @@ EPSILON_DECAY = 0.95 ## tend towards 1.0 depeninding on # of steps
 MIN_EPSILON = 0.001
 
 AGGRIGATE_STATS_EVERY = 10
+
+# Own Tensorboard class
+class ModifiedTensorBoard(TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
 
 class CarEnvironment:
     SHOW_CAMERA = SHOW_PREVIEW
@@ -216,3 +252,72 @@ class CarEnvironment:
             model = Model(inputs=base_model.input, outputs=predictions)
             model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=["accuracy"])
             return model
+
+        def update_replay_memory(self, transition):
+            # transition = (current_state, action, reward, new_state, done)
+            self.replay_memory.append(transition)
+
+        def train(self):
+            if len(self.replay_memory) < MIN_REPLAY_SIZE:
+                return
+
+            minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+            current_states = np.array([transition[0] for transition in minibatch])/255
+            with self.graph.as_default():
+                # predict current Q values for all states in minibatch
+                current_qs_list = self.model.predict(current_states, batch_size=PREDICTION_BATCH_SIZE)
+
+            new_current_states = np.array([transition[3] for transition in minibatch])/255
+            with self.graph.as_default():
+                # predict future Q values for all states in minibatch
+                future_qs_list = self.target_model.predict(new_current_states, batch_size=PREDICTION_BATCH_SIZE)
+
+            X = []
+            y = []
+
+            for index, (current_state, action, reward, new_state, done) in enumerate(minibatch):
+                if not done:
+                    max_future_q = np.max(future_qs_list[index])
+                    new_q = reward + DISCOUNT * max_future_q
+                else:
+                    new_q = reward
+                
+                current_qs = current_qs_list[index]
+                current_qs[action] = new_q
+
+                X.append(current_state)
+                y.append(current_qs)
+                
+            log_this_step = False
+            if self.tensorboard.step > self.last_logged_episode:
+                log_this_step = True
+                self.last_logged_episode = self.tensorboard.step
+
+            with self.graph.as_default():
+                self.model.fit(np.array(X)/255, np.array(y), batch_size=TRAINING_BATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if log_this_step else None)
+
+            if log_this_step:
+                self.target_update_counter += 1
+
+            if self.target_update_counter > UPDATE_TARGET_EVERY:
+                self.target_model.set_weights(self.model.get_weights())
+                self.target_update_counter = 0
+        
+        def get_qs(self, state):
+            return self.model.predict(np.array(state).reshape(-1, IMG_HEIGHT, IMG_WIDTH, 3) / 255)[0]
+            
+        def train_in_loop(self):
+            # first train is always slow, so simulate dummy train
+            X = np.random.uniform(size=(1, IMG_HEIGHT, IMG_WIDTH, 3)).astype(np.float32)
+            y = np.random.uniform(size=(1, 3)).astype(np.float32)
+            with self.graph.as_default():
+                self.model.fit(X, y, batch_size=1, verbose=0)
+            
+            self.training_initialised = True
+
+            while True:
+                if self.terminate:
+                    return
+                self.train()
+                time.sleep(0.01)
